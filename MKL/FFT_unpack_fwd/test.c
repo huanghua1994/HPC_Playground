52733b90d369441cf83470d784c8d277d91beb75
Hua Huang <huangh223@gatech.edu>
2019-03-15 10:19:33 -0400

    Add cuFFT forward unpack demin

--- MKL/FFT_unpack_fwd/test.c
+++ MKL/FFT_unpack_fwd/test.c
@@ -7,7 +7,6 @@
 #include <omp.h>
 
 #include "mkl_dfti.h"
-#include "unpackMKLDftiForward.h"
 
 /*
 MATLAB code:
@@ -15,6 +14,35 @@ x = reshape(mod([1 : nx * ny * nz], 7), [nx, ny, nz]);
 x_hat = fftn(x);
 Then, x_hat(i, j, k) should equals to y[(k-1)*nx*ny + (j-1)*nx + (i-1)] in this code.
 */
+
+// See: https://software.intel.com/sites/default/files/managed/42/5b/Unpack_result_of_Intel_Math_Kernel_Library_Fast_Fourier_Transform_routines_to_align_with_Matlab.pdf
+// Unpack MKL 3D FFT CCE output format after 3D FFT forward transform
+// Use for 2D or 1D output: set nz & ny = 1
+// Input parameters:
+//   n{z, y, x} : Data dimensions, z is the slowest running index and x is the fastest
+//   data       : MKL FFT 3D forward transform output
+// Output parameters:
+//   data       : Unpacked FFT 3D forward transform result
+void unpackMKLDftiForward3DInplace(const int nz, const int ny, const int nx, double _Complex *data)
+{
+    int semi_nx = nx / 2 + 1;
+    int nxny = nx * ny;
+    for (int dst_z = 0; dst_z < nz; dst_z++)
+    {
+        int src_z = (dst_z == 0) ? 0 : (nz - dst_z);
+        for (int dst_y = 0; dst_y < ny; dst_y++)
+        {
+            int src_y = (dst_y == 0) ? 0 : (ny - dst_y);
+            int src_offset = src_z * nxny + src_y * nx;
+            int dst_offset = dst_z * nxny + dst_y * nx;
+            for (int dst_x = semi_nx; dst_x < nx; dst_x++)
+            {
+                int src_x = nx - dst_x;
+                data[dst_offset + dst_x] = conj(data[src_offset + src_x]);
+            }
+        }
+    }
+}
 
 int main()
 {
