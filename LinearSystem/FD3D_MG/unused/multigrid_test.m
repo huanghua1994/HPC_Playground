48848d36b0bf3dff58a2d4f05bbdd5529e89265c
Huang, Hua <huangh223@gatech.edu>
2020-01-28 11:54:22 -0500

    Set up CSRPlus test directory

---
+++ LinearSystem/FD3D_MG/unused/multigrid_test.m
@@ -0,0 +1,145 @@
+function multigrid_test(gridsizes, BCs)
+
+% Along periodic dimensions, must choose N to be even.
+% Along Dirichlet dimensions, there is no restriction on N.
+
+cellDims(1) = (gridsizes(1) - BCs(1)) * 0.4;
+cellDims(2) = (gridsizes(2) - BCs(2)) * 0.4;
+cellDims(3) = (gridsizes(3) - BCs(3)) * 0.4;
+latVecs = eye(3); % not yet handling nonorthogonal lattices
+FDn = 6;
+
+mg = multigrid_setup(cellDims, gridsizes, latVecs, BCs, FDn);
+
+rng(0);
+b = rand(prod(gridsizes),1)-.5;
+
+% if all periodic boundaries, then project b such that the system is consistent
+if norm(BCs) == 0
+  e = ones(size(b));
+  e = e/norm(e);
+  b = b - e*(e'*b);
+end
+
+x0 = zeros(size(b)); % initial guess
+x = multigrid_solve(mg, b, x0);
+
+% solve using perturbed b and previous solution as guess
+% b = b + 0.01*randn(size(b));
+% x0 = x; 
+% x = multigrid_solve(mg, b, x0);
+
+% fprintf('%2d   %e\n', Nx, norm(b-mg.A{1}*x)/norm(b));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function mg = multigrid_setup(cellDims, gridsizes, latVecs, BCs, FDn)
+% Set up the multigrid method for the given FD mesh configuration.
+
+A = GenDiscreteLaplacian(cellDims, gridsizes, latVecs, BCs, FDn);
+mg.A{1} = A;
+
+Nx = gridsizes(1);
+Ny = gridsizes(2);
+Nz = gridsizes(3);
+
+level = 1;
+coarse_size = 7;
+while (Nx > coarse_size && Ny > coarse_size && Nz > coarse_size)
+  [P Nx1 Ny1 Nz1] = prolong(Nx, Ny, Nz, BCs);
+  R = 0.125*P';
+
+  mg.P{level} = P;
+  mg.R{level} = R;
+  mg.M{level} = (1/0.75)*diag(diag(A));
+
+  %Alow = GenDiscreteLaplacian(cellDims, [Nx Ny Nz], latVecs, BCs, FDn);
+  A = R*A*P;
+  mg.A{level+1} = A;
+
+  Nx = Nx1;
+  Ny = Ny1;
+  Nz = Nz1;
+
+  level = level + 1;
+end
+mg.numlevels = level;
+
+if norm(BCs) == 0
+  % if all periodic boundaries, use pseudoinverse for coarsest level solve
+  mg.pinv = pinv(full(mg.A{mg.numlevels}));
+  mg.usepinv = 1;
+else
+  % save cholesky factors for coarsest level solve
+  mg.chol = chol(-mg.A{mg.numlevels}, 'lower');
+  % note sign since our matrix is negative definite
+  mg.usepinv = 0;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function x = multigrid_solve(mg, b, x0)
+% Solve using the multigrid method with right-hand side vector b
+% and initial guess x0.  The structure mg is from multigrid_setup.
+
+x = x0;
+
+% take 40 V-cycles
+for k = 1:40
+
+  r{1} = b-mg.A{1}*x;
+
+  % downward pass
+  for level = 1:mg.numlevels-1
+    e{level} = mg.M{level} \ r{level}; % pre-smoothing
+    r{level+1} = mg.R{level} * (r{level}-mg.A{level}*e{level}); % restrict the residual
+  end
+
+  % solve on the coarsest level
+  if mg.usepinv == 1
+    e{level+1} = mg.pinv * r{mg.numlevels};
+  else
+    e{level+1} = - mg.chol' \ (mg.chol \ r{mg.numlevels}); % note sign
+  end
+
+  % upward pass
+  for level = mg.numlevels-1:-1:1
+    e{level} = e{level} + mg.P{level} * e{level+1}; % prolong the correction
+    e{level} = e{level} + mg.M{level} \ (r{level}-mg.A{level}*e{level}); % post-smoothing
+  end
+
+  x = x + e{1};
+
+  fprintf('%2d   %e\n', k, norm(b-mg.A{1}*x)/norm(b));
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [P Nx1 Ny1 Nz1] = prolong(Nx, Ny, Nz, BCs)
+% 3D prolongator using trilinear interpolation
+% Nx = grid dim in x direction at this level (not global Nx), etc.
+
+e = ones(Nx,1); x = spdiags([e 2*e e], -1:1, Nx, Nx); if BCs(1) == 0, x(1,end)=1; x(end,1)=1; end
+e = ones(Ny,1); y = spdiags([e 2*e e], -1:1, Ny, Ny); if BCs(2) == 0, y(1,end)=1; y(end,1)=1; end
+e = ones(Nz,1); z = spdiags([e 2*e e], -1:1, Nz, Nz); if BCs(3) == 0, z(1,end)=1; z(end,1)=1; end
+P = 0.125*kron(kron(z,y),x);
+
+% select coarse grid points
+cx = 2:2:Nx;
+cy = 2:2:Ny;
+cz = 2:2:Nz;
+
+Nx1 = length(cx);
+Ny1 = length(cy);
+Nz1 = length(cz);
+
+% select columns corresponding to coarse grid points
+cpts = zeros(Nx1*Ny1*Nz1, 1);
+l = 0;
+for i = cz
+for j = cy
+for k = cx
+  l = l + 1;
+  cpts(l) = (i-1)*Ny*Nx + (j-1)*Nx + k;
+end
+end
+end
+P = P(:,cpts);
---
+++ OpenMP/CSRPlus/CSR_SpMV_test.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <omp.h>
+
+#include <mkl.h>
+#include <mkl_spblas.h>
+
+#include "mmio.h"
+#include "utils.h"
+#include "CSRPlus.h"
+
+void read_mtx_file_to_COO_mat(
+    const char *fname, int *nrow_, int *ncol_, int *nnz_,
+    int **row_, int **col_, double **val_
+)
+{
+    FILE *inf = fopen(fname, "r");
+    if (inf == NULL)
+    {
+        printf("Failed to open file %s.\n", fname);
+        exit(1);
+    }
+    
+    MM_typecode matcode;
+    if (mm_read_banner(inf, &matcode) != 0)
+    {
+        printf("Could not process Matrix Market banner.\n");
+        exit(1);
+    }
+    
+    int nrow, ncol, nnz0, nnz;
+    int is_symm = mm_is_symmetric(matcode);
+    int is_comp = mm_is_complex(matcode);
+    int is_patt = mm_is_pattern(matcode);
+    
+    int ret_code = mm_read_mtx_crd_size(inf, &nrow, &ncol, &nnz0);
+    assert(ret_code == 0);
+    nnz = (is_symm) ? nnz0 * 2 : nnz0;
+    
+    int    *row = (int*)    malloc(sizeof(int)    * nnz);
+    int    *col = (int*)    malloc(sizeof(int)    * nnz);
+    double *val = (double*) malloc(sizeof(double) * nnz);
+    assert((row != NULL) && (col != NULL) && (val != NULL));
+    
+    printf("Loading matrix %s: %d rows, %d columns\n", fname, nrow, ncol);
+    printf("Matrix: symmetric = %d, complex = %d, pattern = %d\n", is_symm, is_comp, is_patt);
+    
+    int irow, icol;
+    double ival, imag;
+    
+    // General real / integer matrix
+    if ((is_patt == 0) && (is_symm == 0) && (is_comp == 0))
+    {
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            fscanf(inf, "%lf", &ival);
+            row[i] = irow - 1;
+            col[i] = icol - 1;
+            val[i] = ival;
+        }
+    }
+    
+    // General complex matrix, use only the real part
+    if ((is_patt == 0) && (is_symm == 0) && (is_comp == 1))
+    {
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            fscanf(inf, "%lf", &ival);
+            fscanf(inf, "%lf", &imag);
+            row[i] = irow - 1;
+            col[i] = icol - 1;
+            val[i] = ival;
+        }
+    }
+    
+    // Symmetric real / integer matrix, read once add twice
+    if ((is_patt == 0) && (is_symm == 1) && (is_comp == 0))
+    {
+        nnz = 0;
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            fscanf(inf, "%lf", &ival);
+            row[nnz] = irow - 1;
+            col[nnz] = icol - 1;
+            val[nnz] = ival;
+            nnz++;
+            if (irow != icol)
+            {
+                row[nnz] = icol - 1;
+                col[nnz] = irow - 1;
+                val[nnz] = ival;
+                nnz++;
+            }
+        }
+    }
+    
+    // Symmetric complex matrix, read once add twice, use only the real part
+    if ((is_patt == 0) && (is_symm == 1) && (is_comp == 1))
+    {
+        nnz = 0;
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            fscanf(inf, "%lf", &ival);
+            fscanf(inf, "%lf", &imag);
+            row[nnz] = irow - 1;
+            col[nnz] = icol - 1;
+            val[nnz] = ival;
+            nnz++;
+            if (irow != icol)
+            {
+                row[nnz] = icol - 1;
+                col[nnz] = irow - 1;
+                val[nnz] = ival;
+                nnz++;
+            }
+        }
+    }
+    
+    // General pattern matrix, use 1 as value
+    if ((is_patt == 1) && (is_symm == 0))
+    {
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            row[i] = irow - 1;
+            col[i] = icol - 1;
+            val[i] = 1.0;
+        }
+    }
+    
+    // Symmetric pattern matrix, use 1 as value, read once add twice
+    if ((is_patt == 1) && (is_symm == 1))
+    {
+        nnz = 0;
+        for (int i = 0; i < nnz0; i++)
+        {
+            fscanf(inf, "%d",  &irow);
+            fscanf(inf, "%d",  &icol);
+            row[nnz] = irow - 1;
+            col[nnz] = icol - 1;
+            val[nnz] = 1.0;
+            nnz++;
+            if (irow != icol)
+            {
+                row[nnz] = icol - 1;
+                col[nnz] = irow - 1;
+                val[nnz] = 1.0;
+                nnz++;
+            }
+        }
+    }
+    
+    printf("Load matrix done, %d non-zeros created\n\n", nnz);
+
+    *nrow_ = nrow;
+    *ncol_ = ncol;
+    *nnz_  = nnz;
+    *row_  = row;
+    *col_  = col;
+    *val_  = val;
+}
+
+void test_CSRPlus_SpMV(CSRPlusMatrix_t csrp, const int ntest, const double *x, double *y)
+{
+    double st, et, ut;
+    double GFlops = 2e-9 * (double) csrp->nnz;
+    
+    st = get_wtime_sec();
+    CSRP_partition(omp_get_max_threads(), csrp);
+    CSRP_optimize_NUMA(csrp);
+    et = get_wtime_sec();
+    ut = et - st;
+    printf("CSRPlus setup and optimization done, used time = %.3lf (ms) \n", ut * 1000.0);
+    
+    // Warm up
+    CSRP_SpMV(csrp, x, y);
+    // Real performance test
+    st = get_wtime_sec();
+    for (int k = 0; k < ntest; k++)
+        CSRP_SpMV(csrp, x, y);
+    et = get_wtime_sec();
+    ut = (et - st) / (double) ntest;
+    printf("CSRPlus SpMV done, used time = %.3lf (ms), %lf GFlops\n\n", ut * 1000.0, GFlops / ut);
+}
+
+void test_MKL_IE_SpMV(CSRPlusMatrix_t csrp, const int ntest, const double *x, double *y)
+{
+    double st, et, ut;
+    double GFlops = 2e-9 * (double) csrp->nnz;
+    
+    sparse_matrix_t mkl_sp_mat;
+    struct matrix_descr mat_descr;
+    mat_descr.type = SPARSE_MATRIX_TYPE_GENERAL;
+    mat_descr.diag = SPARSE_DIAG_NON_UNIT;
+    st = get_wtime_sec();
+    mkl_sparse_d_create_csr(
+        &mkl_sp_mat, SPARSE_INDEX_BASE_ZERO, csrp->nrow, csrp->ncol, 
+        csrp->row_ptr, csrp->row_ptr + 1, csrp->col, csrp->val
+    );
+    mkl_sparse_set_mv_hint(mkl_sp_mat, SPARSE_OPERATION_NON_TRANSPOSE, mat_descr, ntest); 
+    mkl_sparse_optimize(mkl_sp_mat);
+    et = get_wtime_sec();
+    ut = et - st;
+    printf("MKL I-E SpMV setup and optimization done, used time = %.3lf (ms) \n", ut * 1000.0);
+    
+    // Warm up
+    mkl_sparse_d_mv(
+        SPARSE_OPERATION_NON_TRANSPOSE, 1.0, mkl_sp_mat, 
+        mat_descr, x, 0.0, y
+    );
+    // Real performance test
+    st = get_wtime_sec();
+    for (int k = 0; k < ntest; k++)
+    {
+        mkl_sparse_d_mv(
+            SPARSE_OPERATION_NON_TRANSPOSE, 1.0, mkl_sp_mat, 
+            mat_descr, x, 0.0, y
+        );
+    }
+    et = get_wtime_sec();
+    ut = (et - st) / (double) ntest;
+    printf("MKL I-E SpMV done, used time = %.3lf (ms), %lf GFlops\n\n", ut * 1000.0, GFlops / ut);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) 
+    {
+        printf("Usage: CSR_SpMV.x <mtx_file>\n");
+        exit(1);
+    }
+    
+    int nrow, ncol, nnz;
+    int *row, *col;
+    double *val;
+    read_mtx_file_to_COO_mat(argv[1], &nrow, &ncol, &nnz, &row, &col, &val);
+    
+    int ntest = (2000000000 / nnz) + 1;
+    if (ntest > 4000) ntest = 4000;
+    
+    double *x  = (double*) malloc(sizeof(double) * ncol);
+    double *y0 = (double*) malloc(sizeof(double) * nrow);
+    double *y1 = (double*) malloc(sizeof(double) * nrow);
+    assert(x  != NULL);
+    assert(y0 != NULL);
+    assert(y1 != NULL);
+    
+    srand48(19241112);
+    for (int i = 0; i < ncol; i++) x[i] = drand48() - 0.5;
+    
+    CSRPlusMatrix_t csrp;
+    CSRP_init_with_COO_matrix(nrow, ncol, nnz, row, col, val, &csrp);
+
+    // Get CSRPlus result
+    test_CSRPlus_SpMV(csrp, ntest, x, y1);
+
+    // Get MKL Inspector-Executor SpMV test result
+    test_MKL_IE_SpMV(csrp, ntest, x, y0);
+
+    double y0_2norm, err_2norm;
+    calc_err_2norm(ncol, y0, y1, &y0_2norm, &err_2norm);
+    printf("Result relative error = %e\n", err_2norm / y0_2norm);
+    
+    free(row);
+    free(col);
+    free(val);
+    free(x);
+    free(y0);
+    free(y1);
+    
+    return 0;
+}
